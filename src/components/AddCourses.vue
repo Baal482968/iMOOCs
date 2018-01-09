1c92619e47ab9bfb0ec99eeaa269392d8c5a68d5
BaalWu <baal482968@gmail.com>
2018-01-09 16:24:15 +0800

    Edit add-course page

--- src/components/AddCourses.vue
+++ src/components/AddCourses.vue
@@ -1,51 +1,64 @@
 <template lang="html">
   <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="addCourseInputGroup1"
-                    label="Course Information:"
-                    label-for="addcourseInput1"
-                    description="Please enter full name of this course">
-        <b-form-select id="addcourseInput1"
-                      :options="types"
-                      required
-                      v-model="form.type">
-        </b-form-select>
-        <b-form-input id="addcourseInput2"
-                      type="text"
-                      v-model="form.name"
-                      required
-                      placeholder="Enter Course Name">
-        </b-form-input>
-        <b-form-file id="file_input1" v-model="img"></b-form-file>
-      </b-form-group>
-      <b-form-group id="exampleInputGroup2"
-                    label="Your Name:"
-                    label-for="exampleInput2">
-        <b-form-input id="exampleInput2"
-                      type="text"
-                      v-model="form.name"
-                      required
-                      placeholder="Enter name">
-        </b-form-input>
-      </b-form-group>
-      <b-form-group id="exampleInputGroup3"
-                    label="Food:"
-                    label-for="exampleInput3">
-        <b-form-select id="exampleInput3"
-                      :options="foods"
-                      required
-                      v-model="form.food">
-        </b-form-select>
-      </b-form-group>
-      <b-form-group id="exampleGroup4">
-        <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
+<b-card bg-variant="light">
+  <b-form-group horizontal
+                breakpoint="lg"
+                label="Add Course"
+                label-size="lg"
+                label-class="font-weight-bold pt-0"
+                class="mb-0">
+    <b-form-group horizontal
+                  label="Select Course:"
+                  label-class="text-sm-right"
+                  label-for="nestedCourse">
+      <b-form-select v-model="course" 
+                  :options="course_options" 
+                  class="mb-3" 
+                  id="nestedCourse"
+                  :aria-invalid="true">
+      </b-form-select>
+      <b-button variant="success">Check Course Status</b-button>
+    </b-form-group>
+    <b-form-group horizontal
+                  label="Class Name:"
+                  label-class="text-sm-right"
+                  label-for="nestedName">
+      <b-form-input id="nestedName" v-model="cls_name"></b-form-input>
+    </b-form-group>
+    <b-form-group horizontal
+                  label="Class Content:"
+                  label-class="text-sm-right"
+                  label-for="nestedContent">
+      <b-form-textarea id="nestedContent"
+                  v-model="cls_content"
+                  placeholder="Enter something"
+                  :rows="5"
+                  :max-rows="10">
+      </b-form-textarea>
+    </b-form-group>
+    <b-form-group horizontal
+                  label="Class URL:"
+                  label-class="text-sm-right"
+                  label-for="nestedURL">
+      <b-form-input id="nestedURL"></b-form-input>
+    </b-form-group>
+    <b-form-group horizontal
+                  label="Upload Image:"
+                  label-class="text-sm-right"
+                  label-for="nestedImage">
+      <b-form-file id="nestedImage" v-model="cls_img"></b-form-file>
+    </b-form-group>
+    <b-form-group horizontal
+                  label="Episode Type:"
+                  label-class="text-sm-right"
+                  class="mb-0">
+      <b-form-radio-group class="pt-2" :options="['Regular', 'Final', 'Special']" />
+    </b-form-group>
+    <br/>
+    <b-button type="submit" variant="primary" :disabled="true">Submit</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
+  </b-form-group>
+</b-card>
   </div>
 </template>
 
@@ -53,17 +66,15 @@
 export default {
   data () {
     return {
-      form: {
-        type: '',
-        name: '',
-        food: null,
-        checked: []
-      },
-      img: '',
-      types: [
-        { text: 'Select type', value: null },
+      course: null,
+      course_options: [
+        { text: 'Select Course', value: null },
         'Data Base', 'System Analysis Design', 'System Engineering', 'Python'
       ],
+      cls_name: '',
+      cls_content: '',
+      cls_url: '',
+      cls_img: '',
       show: true
     }
   },
